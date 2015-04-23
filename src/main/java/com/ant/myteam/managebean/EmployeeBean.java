9636b37ca85fb787037659211113e508edb0deb4
mzerek <maciek.zerek@gmail.com>
2015-04-23 12:54:18 +0200

    jms config

--- src/main/java/com/ant/myteam/managebean/EmployeeBean.java
+++ src/main/java/com/ant/myteam/managebean/EmployeeBean.java
@@ -1,9 +1,17 @@
 package com.ant.myteam.managebean;
 
+import java.io.File;
 import java.io.Serializable;
 
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
 import org.springframework.stereotype.Component;
+import org.springframework.util.FileSystemUtils;
 
 import com.ant.myteam.model.Employee;
 import com.ant.myteam.service.EmployeeService;
@@ -13,10 +21,16 @@ public class EmployeeBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	static String mailboxDestinationOne = "mailbox-destination-one";
+	static String mailboxDestinationTwo = "mailbox-destination-two";
+
 	private Employee employee = new Employee();
 
 	@Autowired
 	private EmployeeService employeeService;
+
+	@Autowired
+	private JmsTemplate jmsTemplate;
 
 	private Employee emp1;
 	private Employee emp2;
@@ -34,6 +48,8 @@ public class EmployeeBean implements Serializable {
 		employeeService.addEmployee(emp1);
 		employeeService.addEmployee(emp2);
 		employee = employeeService.findEmployeeById(emp1.getEmpId());
+
+		sendMessageForTwo();
 	}
 
 	public Employee getEmployee() {
@@ -42,6 +58,22 @@ public class EmployeeBean implements Serializable {
 
 	public void setEmployee(Employee employee) {
 		this.employee = employee;
+	}
+
+	private void sendMessageForTwo() {
+		FileSystemUtils.deleteRecursively(new File("activemq-data"));
+
+		// Send a message
+		MessageCreator messageCreator = new MessageCreator() {
+			@Override
+			public Message createMessage(Session session) throws JMSException {
+				// session.createTopic("fajnyOne");
+				return session.createTextMessage("Message from AppOne!");
+			}
+		};
+
+		System.out.println("Sending a new message from AppOne.");
+		jmsTemplate.send(mailboxDestinationOne, messageCreator);
 	}
 
 }
